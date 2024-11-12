7c77fa7d3170b743c02a2fa4445974850678f912
Amirabdollahi773 <amirabdollahi773@gmail.com>
2024-11-12 11:57:42 +0330

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(App):
+        return Label(text='Hello World')
+if __name__=='__main__':
+    MyApp().run()
